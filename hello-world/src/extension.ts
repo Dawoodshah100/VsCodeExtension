2b9c737ef53e21b4c580dbddac97898c404cdf88
Dawood Shah <dawoodshah100@gmail.com>
2022-07-21 20:33:46 +0500

    Three commands addition

--- hello-world/src/extension.ts
+++ hello-world/src/extension.ts
@@ -5,23 +5,54 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "hello-world" is now active!');
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
+	// Now provide the implementatiob n of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hello-world.helloWorld', () => {
+	let helloWorldDisposable = vscode.commands.registerCommand('hello-world.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Hello World!');
-		vscode.window.showOpenDialog();
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			editor.edit((editBuilder) => {
+				editBuilder.delete(editor!.selection);
+			}).then((editBuilder) => {
+				editor!.edit((editBuilder) => {
+					editBuilder.insert(editor!.selection.start, "Hello World!");
+				});
+			});
+		}
+		else{
+			vscode.window.showErrorMessage('No file is open!');
+			vscode.window.showInputBox();
+			
+		}
+
+		// vscode.window.showOpenDialog();
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(helloWorldDisposable);
+
+	let helloWorldDisposable2 = vscode.commands.registerCommand('hello-world.helloWorld2', () => {
+			vscode.window.showInformationMessage('Hello World from Hello World222222222!');
+	});
+
+	context.subscriptions.push(helloWorldDisposable2);
+
+	let helloWorldDisposable3 = vscode.commands.registerCommand('hello-world.helloWorld3', () => {
+
+		vscode.window.showInformationMessage('Hello World from Hello World333333333!');
+
+	});
+
+	context.subscriptions.push(helloWorldDisposable3);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
